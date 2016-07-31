c739bf5ac25e6457f6cf7166b3d307696eed9d6e
Chris Warrick <kwpolska@gmail.com>
2016-07-31 10:46:11 +0200

    Add DOCTYPE, newlines to app output
    Signed-off-by: Chris Warrick <kwpolska@gmail.com>

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,16 @@
+# A simple Flask demo app that demonstrates one basic route, favicons,
+# and static files. Written for my tutorial,
+# Deploying Python Web Applications with nginx and uWSGI Emperor.
+# <https://go.chriswarrick.com/pyweb>
+# Licensed under the WTFPL.
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return '<h1>Hello from Flask!</h1><img src="/static/hello.png">'
+    return ('<!DOCTYPE html>\n'
+            '<meta charset="utf-8">\n'
+            '<title>Hello from Flask!</title>\n'
+            '<h1>Hello from Flask!</h1><img src="/static/hello.png">\n')
